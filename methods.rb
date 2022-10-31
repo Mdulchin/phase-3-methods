1d5068260f5de3b548a28ef73dce04ba2c0cd07f
madelinedulchin <madeline.dulchin@gmail.com>
2022-10-31 11:34:13 -0400

    commit tree 4a2b47bd9c3348460b2ea6c914ea5c6a79268356

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+def add(num1, num2)
+    num1 + num2
+end
+
+add 2,3
+
+
+def halve(num1)
+    if num1.class != Integer
+        return nil
+    end
+    num1 / 2
+end
